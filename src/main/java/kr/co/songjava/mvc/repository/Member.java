af7bd200cc456e4ba2b66eb2644cb45a8bff2098
Soojin <psjinqkr23@gmail.com>
2022-01-10 00:52:03 +0900

    springLab-14 springLab - JPA 기본사용법

--- src/main/java/kr/co/songjava/mvc/repository/Member.java
+++ src/main/java/kr/co/songjava/mvc/repository/Member.java
@@ -1,20 +1,29 @@
 package kr.co.songjava.mvc.repository;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Data
+@Setter
+@Getter
+@ToString
 @Entity(name = "T_MEMBER") //실제 테이블명과 클래스명이 다를경우
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int memberSeq;
+
     @Column(name = "MEMBER_ID")
     private String memberId;
 
 
     @Enumerated(EnumType.STRING)
+    @Column(name="MEMBER_TYPE")
     private MemberType memberType;
     @Column(name = "PASSWORD")
     private String password;
@@ -24,8 +33,11 @@ public class Member {
     private String companyName;
     @Column(name = "DEPART_NAME")
     private String departName;
+    @Column(name = "EMAIL")
     private String email;
+    @Column(name = "TEL_NUMBER")
     private String telNumber;
+    @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
     private String zipcode;
     private String address;
