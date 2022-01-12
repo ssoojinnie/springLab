b827ca51486a9b0c8c082c1e819473f3b836a5b9
Soojin <psjinqkr23@gmail.com>
2022-01-12 18:30:15 +0900

    springLab-16 springLab - thymeleaf (+자동완성)

--- src/main/java/kr/co/songjava/examplespring/ExampleSpringApplication.java
+++ src/main/java/kr/co/songjava/examplespring/ExampleSpringApplication.java
@@ -6,14 +6,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "kr.co.songjava")
 @EnableScheduling
+@Controller
 public class ExampleSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleSpringApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home(Model model, @RequestParam String menu){
+		model.addAttribute("menu", menu);
+		model.addAttribute("name", "자동완성전 text");
+		model.addAttribute("values", Arrays.asList("Spring", "Spring Boot", "Vue.JS", "React"));
+		return "thymeleaf/example1";
+	}
 }
