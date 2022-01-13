6f201ed0bbb3f14a9e25b8e4b45de7640b30a2be
Soojin <psjinqkr23@gmail.com>
2022-01-13 23:33:43 +0900

    springLab-20 VueJS 게시판 목록(jsp -> BaseResponse<List<Board>>)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="list-group">
+      <a v-for="board in data.boardList" v-bind:key="board.boardSeq" class="list-group-item list-group-item-action active" aria-current="true">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{board.boardType.label}}</h5>
+          <small>{{board.regDt}}</small>
+        </div>
+        <p class="mb-1">{{board.title}}</p>
+        <small>{{board.contents}}</small>
+      </a>
+      <a v-if="data.boardList.length == 0" class="list-group-item list-group-item-action active" aria-current="true">
+        게시물이 없습니다.
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, reactive } from 'vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  setup() {
+    const data = reactive({
+      boardList: []
+    });
+    const getList = () => {
+      fetch('http://localhost:9000/faq')
+      .then(response => response.json())
+      .then(response => {
+      data.boardList = response.data;
+      });
+    };
+    onMounted(() => {
+      getList();
+    });
+    return {
+      data: data,
+      getList: getList
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
 </style>
